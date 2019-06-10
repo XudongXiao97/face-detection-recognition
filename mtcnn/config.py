0c8cc7bcea197b7eee1ed22984bc4a44ac90468b
jeff.zhu <jeff.zhu@gdsinoyue.com>
2019-06-10 13:52:21 +0800

    删除冗余版本

--- mtcnn/config.py
+++
@@ -1,6 +0,0 @@
-VIDEO_PATH = "/home/vision/Downloads/0314_face/192.168.1.169.mp4"
-GPU_MEMORY_LIMIT = 0.5
-OUTPUT_FACE_DICT = {
-    "face": "/mnt/HD_2TB/hua/github/face-detection-recognition/mtcnn/output/face",
-    "noface": "/mnt/HD_2TB/hua/github/face-detection-recognition/mtcnn/output/noface"
-}
